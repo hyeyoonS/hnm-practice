695068cdb32cd604c3f019b8e3614a876cbb8ab8
Hyeyoon <foryou9097@gmail.com>
2023-12-05 22:26:48 +0900

    feat : axios로 ProductAll 상품데이터 가져오기

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const ProductAll = () => {
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products`;
+    let response = await axios.get(url);
+    let data = response.data;
+    console.log(data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
   return <div>전체상품페이지</div>;
 };
 
