e73ef8c0918fadb83377c01755488ee4961cbec6
Hyeyoon <foryou9097@gmail.com>
2023-12-06 14:17:11 +0900

    feat : ProductCard만들기

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fd4%2F06%2Fd406586d8d04d4a51bce2f8aeaecd04d10ed467e.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]" />
+      <div>Conscious choice</div>
+      <div>벨티드 트윌 코트</div>
+      <div>99000원</div>
+      <div>신제품</div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
+  const [productList, setProductList] = useState([]);
   const getProducts = async () => {
     let url = `http://localhost:5000/products`;
     let response = await axios.get(url);
     let data = response.data;
-    console.log(data);
+    setProductList(data);
   };
   useEffect(() => {
     getProducts();
   }, []);
-  return <div>전체상품페이지</div>;
+  return (
+    <div>
+      전체상품페이지
+      <ProductCard />
+    </div>
+  );
 };
 
 export default ProductAll;
