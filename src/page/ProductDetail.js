d9c3d321a7da7355240620efdb435a470171991a
HyeyoonS <foryou9097@gmail.com>
2023-12-09 21:33:48 +0900

    feat : useParams로 id값을 읽어서 해당 API데이터 개별적으로 가져오기

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import { useParams } from 'react-router-dom/dist';
 
-const ProductDetail = () => {
+
+const ProductDetail = () =>{
+  let {id} = useParams();
+  const getProductDetail= async()=>{
+    let url = `http://localhost:5000/products/${id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+  }
+  useEffect(()=>{
+    getProductDetail();
+  },[])
+
+
   return <div>상품상세페이지</div>;
 };
 
