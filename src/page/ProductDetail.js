a44998d42e3417d3f821e999c3f153b658ac1079
HyeyoonS <foryou9097@gmail.com>
2023-12-09 21:47:37 +0900

    feat : ProductDetail의  API정보 받아오기

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,21 +1,35 @@
-import React,{useEffect} from 'react';
+import React,{useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom/dist';
+import styled from 'styled-components';
 
 
 const ProductDetail = () =>{
+  let [product, setProduct] = useState(null);
   let {id} = useParams();
   const getProductDetail= async()=>{
     let url = `http://localhost:5000/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
-    console.log(data);
+    setProduct(data)
   }
   useEffect(()=>{
     getProductDetail();
   },[])
 
 
-  return <div>상품상세페이지</div>;
+  return <div>
+    <div>
+      <Img src={product?.img}/>
+      <div>{product?.choice === true ? "Conscious choice" : ""}</div>
+      <div>{product?.title}</div>
+      <div>{product?.price}</div>
+      <div>{product?.new === true? "신제품": ""}</div>
+    </div>
+
+  </div>;
 };
 
 export default ProductDetail;
+
+const Img = styled.img`
+`
